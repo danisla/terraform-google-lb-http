77ba619547c457f82851d6fa3635c51a350e8a39
Dan Isla <disla@google.com>
2018-08-24 20:45:28 +0000

    truncate certificate name prefix so that it doesn't execeed 37 chars

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "google_compute_target_https_proxy" "default" {
 resource "google_compute_ssl_certificate" "default" {
   project     = "${var.project}"
   count       = "${(var.ssl && !var.use_ssl_certificates) ? 1 : 0}"
-  name_prefix = "${var.name}-certificate-"
+  name_prefix = "${substr(var.name, 0, 24)}-certificate-"
   private_key = "${var.private_key}"
   certificate = "${var.certificate}"
 
